3ff39364388a1c304c09e793d3c1acb694bf4a3b
Anand Kane <anandkane@yahoo.com>
2018-02-03 17:02:10 +0530

    Find longest non repeating sequesnce

---
+++ src/test/java/org/spearhead/tinybits/algorythm/LongestNonRepeatingSeqTest.java
@@ -0,0 +1,43 @@
+package org.spearhead.tinybits.algorythm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestNonRepeatingSeqTest {
+
+	@Test
+	public void testSimplest() {
+		String result;
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("qwert");
+		assertEquals("qwert", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asda");
+		assertEquals("asd", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asdaf");
+		assertEquals("asd", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asdafg");
+		assertEquals("asd", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asdafgh");
+		assertEquals("afgh", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asdafghjakl");
+		assertEquals("afghj", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asdafaghjakl");
+		assertEquals("aghj", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("asdafghjaklpoiu");
+		assertEquals("aklpoiu", result);
+	}
+
+	@Test
+	public void testComplex() {
+		String result;
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("aas");
+		assertEquals("as", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("aaas");
+		assertEquals("as", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("aaass");
+		assertEquals("as", result);
+		result = LongestNonRepeatingSeq.getLongestNonRepeatingSeq("waedftaedaty");
+		assertEquals("aedft", result);
+	}
+}
